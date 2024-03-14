32762c7d8e8d4a9508c85e6e41e005f48d9a2090
Dani <alexanderdanailin@gmail.com>
2024-03-14 21:33:49 +0100

    Startup Fix

--- Login/LoginPage.xaml.cs
+++ Login/LoginPage.xaml.cs
@@ -1,4 +1,3 @@
-using GameBib.Login;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -14,15 +13,19 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
-namespace GameBib
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace GameBib.Login
 {
-    public sealed partial class MainWindow : Window
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page
     {
-        public MainWindow()
+        public LoginPage()
         {
             this.InitializeComponent();
-
-            this.rootFrame.Navigate(typeof(LoginPage));
         }
     }
 }
---
+++ Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBib.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Game> Games { get; set; }
+        public ICollection<FavoritedGame> FavoritedGames { get; set; }
+    }
+}
