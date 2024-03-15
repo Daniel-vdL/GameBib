4e394f05b7d0218ce51f0caacc9cd48670478011
justinvanderhoeven <74967919+justinvanderhoeven@users.noreply.github.com>
2024-03-15 11:45:48 +0100

    Added the rest of pages
    all pages of the application are added,
    also startes on the gamelistview for the user

---
+++ OtherPages/GameViewPageUser.xaml.cs
@@ -0,0 +1,80 @@
+using ABI.System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using GameBib.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.Json;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using static System.Net.WebRequestMethods;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace GameBib.OtherPages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class GameViewPageUser : Page
+    {
+        public GameViewPageUser()
+        {
+            this.InitializeComponent();
+
+            LoadGames();
+        }
+
+        public async void LoadGames()
+        {
+
+            string url = "https://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json";
+
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            var games = JsonSerializer.Deserialize<List<Game>>(content, options);
+
+            GamesListView.ItemsSource = games;
+
+        }
+
+        private void SearchGames_Click(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(DashboardPage));
+        }
+
+        private void GamesListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var selectedGame = (Game)e.ClickedItem;
+
+            //this.Frame.Navigate(typeof(GameInfoPage), selectedGame.Id);
+        }
+
+        private async void GamesListView_RightTapped(object sender, RoutedEventArgs e)
+        {
+            //
+        }
+    }
+}
