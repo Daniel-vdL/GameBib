3de6ee40517a9a65dd58e1cb78ee865f4ef1754e
Dani <alexanderdanailin@gmail.com>
2024-03-22 16:34:45 +0100

    Function For Changing Username

--- OtherPages/AccountPage.xaml.cs
+++ OtherPages/AccountPage.xaml.cs
@@ -1,3 +1,4 @@
+using GameBib.Models;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -9,7 +10,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -26,5 +29,47 @@ namespace GameBib.OtherPages
         {
             this.Frame.Navigate(typeof(DashboardPage));
         }
+
+        private async void NameChangeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var username = UsernameTextbox.Text;
+
+            if (!string.IsNullOrWhiteSpace(username))
+            {
+                using var client = new HttpClient();
+
+                var user = new User
+                {
+                    Id = User.CurrentUser.Id,
+                    Username = username
+                };
+
+                var userJson = JsonSerializer.Serialize(user);
+                var context = new StringContent(userJson, System.Text.Encoding.UTF8, "application/json");
+
+                try
+                {
+                    var response = await client.PutAsync($"https://localhost:7063/api/Users/{User.CurrentUser.Id}", context);
+
+                    // Check if the request was successful
+                    if (response.IsSuccessStatusCode)
+                    {
+                        User.CurrentUser = user;
+                        this.Frame.Navigate(typeof(AccountPage));
+                    }
+                    else
+                    {
+                        // Handle the failure scenario
+                        // For example, display an error message to the user
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Handle any exceptions that might occur during the API call
+                    // For example, log the exception or display an error message to the user
+                }
+            }
+        }
+
     }
 }
