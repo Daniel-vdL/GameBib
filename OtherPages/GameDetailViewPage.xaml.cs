5fff6dc10fb16461eff02f5dc1be806366fbdbb1
justinvanderhoeven <74967919+justinvanderhoeven@users.noreply.github.com>
2024-03-24 20:20:00 +0100

    Update GameDetailViewPage.xaml.cs
    This code works, but the app wont.. Needs a review

--- OtherPages/GameDetailViewPage.xaml.cs
+++ OtherPages/GameDetailViewPage.xaml.cs
@@ -12,6 +12,9 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System.Text.Json;
+using System.Net.Http;
+using GameBib.Models;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +26,54 @@ namespace GameBib.OtherPages
     /// </summary>
     public sealed partial class GameDetailViewPage : Page
     {
+        private Models.App selectedGame;
+
         public GameDetailViewPage()
         {
             this.InitializeComponent();
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (e.Parameter is Models.App game)
+            {
+                selectedGame = game;
+                LoadAppDetail();
+            }
+        }
+
+        public async void LoadAppDetail()
+        {
+            if (selectedGame == null)
+                return;
+
+            string url = $"https://store.steampowered.com/api/appdetails?appids={selectedGame.appid}";
+
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            var appDetail = JsonSerializer.Deserialize<Dictionary<string, AppDetails>>(content, options);
+
+            if (appDetail != null && appDetail.ContainsKey(selectedGame.appid.ToString()))
+            {
+                var gameDetail = appDetail[selectedGame.appid.ToString()];
+                gameDetailsListView.ItemsSource = new List<AppDetails> { gameDetail };
+            }
+        }
+
+        private void returnButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(GameViewPageUser));
+        }
     }
+
 }
+
